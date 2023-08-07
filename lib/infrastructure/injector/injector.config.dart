c20f8adcec0c3e5ea305999bbdf096ae7616402d
bytes7bytes7 <bytes7bytes7@gmail.com>
2023-08-07 15:39:30 +0300

    move padding in ForecastScreen

--- lib/infrastructure/injector/injector.config.dart
+++ lib/infrastructure/injector/injector.config.dart
@@ -10,68 +10,68 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:dio/dio.dart' as _i9;
-import 'package:firebase_auth/firebase_auth.dart' as _i19;
+import 'package:firebase_auth/firebase_auth.dart' as _i28;
 import 'package:flutter/material.dart' as _i13;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../../application/application.dart' as _i4;
-import '../../application/blocs/auth/auth_bloc.dart' as _i63;
+import '../../application/blocs/auth/auth_bloc.dart' as _i67;
 import '../../application/blocs/forecast/forecast_bloc.dart' as _i69;
-import '../../application/blocs/splash/splash_bloc.dart' as _i62;
-import '../../application/coordinators/auth_coordinator.dart' as _i40;
-import '../../application/mappers/forecast_to_forecast_vm_mapper.dart' as _i59;
+import '../../application/blocs/splash/splash_bloc.dart' as _i66;
+import '../../application/coordinators/auth_coordinator.dart' as _i43;
+import '../../application/mappers/forecast_to_forecast_vm_mapper.dart' as _i63;
 import '../../application/mappers/temperature_to_temperature_vm_mapper.dart'
-    as _i56;
-import '../../application/mappers/weather_to_weather_vm_mapper.dart' as _i53;
-import '../../application/mappers/wind_to_wind_vm_mapper.dart' as _i50;
+    as _i61;
+import '../../application/mappers/weather_to_weather_vm_mapper.dart' as _i58;
+import '../../application/mappers/wind_to_wind_vm_mapper.dart' as _i55;
 import '../../application/providers/auth_exception_string_provider.dart'
-    as _i64;
+    as _i68;
 import '../../application/providers/date_time_provider.dart' as _i6;
-import '../../application/providers/date_time_string_provider.dart' as _i61;
+import '../../application/providers/date_time_string_provider.dart' as _i65;
 import '../../application/providers/forecast_exception_string_provider.dart'
     as _i70;
-import '../../application/providers/humidity_string_provider.dart' as _i60;
-import '../../application/providers/splash_string_provider.dart' as _i32;
-import '../../application/providers/temperature_string_provider.dart' as _i34;
-import '../../application/providers/weather_icon_provider.dart' as _i54;
+import '../../application/providers/humidity_string_provider.dart' as _i64;
+import '../../application/providers/splash_string_provider.dart' as _i35;
+import '../../application/providers/temperature_string_provider.dart' as _i37;
+import '../../application/providers/weather_icon_provider.dart' as _i59;
 import '../../application/providers/wind_direction_string_provider.dart'
-    as _i51;
-import '../../application/providers/wind_string_provider.dart' as _i38;
+    as _i56;
+import '../../application/providers/wind_string_provider.dart' as _i41;
 import '../../application/view_models/temperature_vm/temperature_vm.dart'
-    as _i55;
-import '../../application/view_models/view_models.dart' as _i58;
-import '../../application/view_models/weather_vm/weather_vm.dart' as _i52;
-import '../../application/view_models/wind_vm/wind_vm.dart' as _i49;
-import '../../domain/entities/account.dart' as _i20;
-import '../../domain/repositories/auth_repository.dart' as _i42;
-import '../../domain/repositories/forecast_repository.dart' as _i65;
+    as _i60;
+import '../../application/view_models/view_models.dart' as _i62;
+import '../../application/view_models/weather_vm/weather_vm.dart' as _i57;
+import '../../application/view_models/wind_vm/wind_vm.dart' as _i54;
+import '../../domain/entities/account.dart' as _i29;
+import '../../domain/repositories/auth_repository.dart' as _i45;
+import '../../domain/repositories/forecast_repository.dart' as _i49;
 import '../../domain/repositories/location_repository.dart' as _i16;
-import '../../domain/services/auth_service.dart' as _i45;
-import '../../domain/services/forecast_service.dart' as _i68;
-import '../../domain/value_objects/forecast.dart' as _i47;
-import '../../domain/value_objects/temperature.dart' as _i23;
-import '../../domain/value_objects/value_objects.dart' as _i57;
-import '../../domain/value_objects/weather.dart' as _i26;
-import '../../domain/value_objects/wind.dart' as _i29;
+import '../../domain/services/auth_service.dart' as _i48;
+import '../../domain/services/forecast_service.dart' as _i53;
+import '../../domain/value_objects/forecast.dart' as _i32;
+import '../../domain/value_objects/temperature.dart' as _i26;
+import '../../domain/value_objects/value_objects.dart' as _i52;
+import '../../domain/value_objects/weather.dart' as _i23;
+import '../../domain/value_objects/wind.dart' as _i20;
 import '../../utils/mapper.dart' as _i18;
-import '../coordinators/auth_coordinator.dart' as _i41;
-import '../data_sources/data_sources.dart' as _i44;
+import '../coordinators/auth_coordinator.dart' as _i44;
+import '../data_sources/data_sources.dart' as _i47;
 import '../data_sources/firebase_data_source.dart' as _i10;
 import '../data_sources/geocoding_data_source/geocoding_data_source.dart'
     as _i12;
 import '../data_sources/location_data_source.dart' as _i15;
-import '../data_sources/open_weather/open_weather_data_source.dart' as _i31;
-import '../dto/dto.dart' as _i67;
-import '../dto/forecast_response/forecast_response.dart' as _i46;
-import '../dto/weather_info_response/weather_info_response.dart' as _i22;
-import '../dto/weather_response/weather_response.dart' as _i25;
-import '../dto/wind_response/wind_response.dart' as _i28;
-import '../mappers/forecast_response_to_forecast_mapper.dart' as _i48;
-import '../mappers/user_to_account_mapper.dart' as _i21;
-import '../mappers/weather_info_response_to_temperature_mapper.dart' as _i24;
-import '../mappers/weather_response_to_weather_mapper.dart' as _i27;
-import '../mappers/wind_response_to_wind_mapper.dart' as _i30;
+import '../data_sources/open_weather/open_weather_data_source.dart' as _i34;
+import '../dto/dto.dart' as _i51;
+import '../dto/forecast_response/forecast_response.dart' as _i31;
+import '../dto/weather_info_response/weather_info_response.dart' as _i25;
+import '../dto/weather_response/weather_response.dart' as _i22;
+import '../dto/wind_response/wind_response.dart' as _i19;
+import '../mappers/forecast_response_to_forecast_mapper.dart' as _i33;
+import '../mappers/user_to_account_mapper.dart' as _i30;
+import '../mappers/weather_info_response_to_temperature_mapper.dart' as _i27;
+import '../mappers/weather_response_to_weather_mapper.dart' as _i24;
+import '../mappers/wind_response_to_wind_mapper.dart' as _i21;
 import '../modules/dio_module.dart' as _i71;
 import '../modules/hive_module.dart' as _i3;
 import '../modules/navigator_key_module.dart' as _i72;
@@ -80,13 +80,13 @@ import '../providers/date_time_provider.dart' as _i7;
 import '../providers/date_time_string_provider.dart' as _i8;
 import '../providers/forecast_exception_string_provider.dart' as _i11;
 import '../providers/humidity_string_provider.dart' as _i14;
-import '../providers/splash_string_provider.dart' as _i33;
-import '../providers/temperature_string_provider.dart' as _i35;
-import '../providers/weather_icon_provider.dart' as _i36;
-import '../providers/wind_direction_string_provider.dart' as _i37;
-import '../providers/wind_string_provider.dart' as _i39;
-import '../repositories/auth_repository.dart' as _i43;
-import '../repositories/forecast_repository.dart' as _i66;
+import '../providers/splash_string_provider.dart' as _i36;
+import '../providers/temperature_string_provider.dart' as _i38;
+import '../providers/weather_icon_provider.dart' as _i39;
+import '../providers/wind_direction_string_provider.dart' as _i40;
+import '../providers/wind_string_provider.dart' as _i42;
+import '../repositories/auth_repository.dart' as _i46;
+import '../repositories/forecast_repository.dart' as _i50;
 import '../repositories/location_repository.dart' as _i17;
 
 const String _test = 'test';
@@ -150,98 +150,98 @@ Future<_i1.GetIt> init(
     },
     preResolve: true,
   );
-  gh.lazySingleton<_i18.Mapper<_i19.User, _i20.Account>>(
-      () => _i21.UserToAccountMapper());
-  gh.lazySingleton<_i18.Mapper<_i22.WeatherInfoResponse, _i23.Temperature>>(
-      () => _i24.WeatherInfoResponseToTemperatureMapper());
-  gh.lazySingleton<_i18.Mapper<_i25.WeatherResponse, _i26.Weather>>(
-      () => _i27.WeatherResponseToWeatherMapper());
-  gh.lazySingleton<_i18.Mapper<_i28.WindResponse, _i29.Wind>>(
-      () => _i30.WindResponseToWindMapper());
-  gh.lazySingleton<_i31.OpenWeatherDataSource>(
-      () => _i31.OpenWeatherDataSource(gh<_i9.Dio>()));
-  gh.lazySingleton<_i32.SplashStringProvider>(
-      () => _i33.ProdSplashStringProvider());
-  gh.lazySingleton<_i34.TemperatureStringProvider>(
-      () => _i35.ProdTemperatureStringProvider());
+  gh.lazySingleton<_i18.Mapper<_i19.WindResponse, _i20.Wind>>(
+      () => _i21.WindResponseToWindMapper());
+  gh.lazySingleton<_i18.Mapper<_i22.WeatherResponse, _i23.Weather>>(
+      () => _i24.WeatherResponseToWeatherMapper());
+  gh.lazySingleton<_i18.Mapper<_i25.WeatherInfoResponse, _i26.Temperature>>(
+      () => _i27.WeatherInfoResponseToTemperatureMapper());
+  gh.lazySingleton<_i18.Mapper<_i28.User, _i29.Account>>(
+      () => _i30.UserToAccountMapper());
+  gh.lazySingleton<_i18.Mapper<_i31.ForecastResponse, _i32.Forecast>>(
+      () => _i33.ForecastResponseToForecastMapper(
+            gh<_i18.Mapper<_i19.WindResponse, _i20.Wind>>(),
+            gh<_i18.Mapper<_i22.WeatherResponse, _i23.Weather>>(),
+            gh<_i18.Mapper<_i25.WeatherInfoResponse, _i26.Temperature>>(),
+          ));
+  gh.lazySingleton<_i34.OpenWeatherDataSource>(
+      () => _i34.OpenWeatherDataSource(gh<_i9.Dio>()));
+  gh.lazySingleton<_i35.SplashStringProvider>(
+      () => _i36.ProdSplashStringProvider());
+  gh.lazySingleton<_i37.TemperatureStringProvider>(
+      () => _i38.ProdTemperatureStringProvider());
   gh.lazySingleton<_i4.WeatherIconProvider>(
-      () => _i36.ProdWeatherIconProvider());
+      () => _i39.ProdWeatherIconProvider());
   gh.lazySingleton<_i4.WindDirectionStringProvider>(
-      () => _i37.ProdWindDirectionStringProvider());
-  gh.lazySingleton<_i38.WindStringProvider>(
-      () => _i39.ProdWindStringProvider());
-  gh.lazySingleton<_i40.AuthCoordinator>(() =>
-      _i41.ProdAuthCoordinator(gh<_i13.GlobalKey<_i13.NavigatorState>>()));
-  gh.lazySingleton<_i42.AuthRepository>(
-    () => _i43.ProdAuthRepository(
-      gh<_i44.FirebaseDataSource>(),
-      gh<_i18.Mapper<_i19.User, _i20.Account>>(),
+      () => _i40.ProdWindDirectionStringProvider());
+  gh.lazySingleton<_i41.WindStringProvider>(
+      () => _i42.ProdWindStringProvider());
+  gh.lazySingleton<_i43.AuthCoordinator>(() =>
+      _i44.ProdAuthCoordinator(gh<_i13.GlobalKey<_i13.NavigatorState>>()));
+  gh.lazySingleton<_i45.AuthRepository>(
+    () => _i46.ProdAuthRepository(
+      gh<_i47.FirebaseDataSource>(),
+      gh<_i18.Mapper<_i28.User, _i29.Account>>(),
     ),
     registerFor: {
       _test,
       _prod,
     },
   );
-  await gh.singletonAsync<_i45.AuthService>(
+  await gh.singletonAsync<_i48.AuthService>(
     () {
-      final i = _i45.AuthService(gh<_i42.AuthRepository>());
+      final i = _i48.AuthService(gh<_i45.AuthRepository>());
       return i.init().then((_) => i);
     },
     preResolve: true,
     dispose: (i) => i.dispose(),
   );
-  gh.lazySingleton<_i18.Mapper<_i46.ForecastResponse, _i47.Forecast>>(
-      () => _i48.ForecastResponseToForecastMapper(
-            gh<_i18.Mapper<_i28.WindResponse, _i29.Wind>>(),
-            gh<_i18.Mapper<_i25.WeatherResponse, _i26.Weather>>(),
-            gh<_i18.Mapper<_i22.WeatherInfoResponse, _i23.Temperature>>(),
-          ));
-  gh.lazySingleton<_i18.Mapper<_i29.Wind, _i49.WindVM>>(
-      () => _i50.WindToWindVMMapper(
-            gh<_i51.WindDirectionStringProvider>(),
-            gh<_i38.WindStringProvider>(),
-          ));
-  gh.lazySingleton<_i18.Mapper<_i26.Weather, _i52.WeatherVM>>(
-      () => _i53.WeatherToWeatherVMMapper(gh<_i54.WeatherIconProvider>()));
-  gh.lazySingleton<_i18.Mapper<_i23.Temperature, _i55.TemperatureVM>>(() =>
-      _i56.TemperatureToTemperatureVMMapper(
-          gh<_i34.TemperatureStringProvider>()));
-  gh.lazySingleton<_i18.Mapper<_i57.Forecast, _i58.ForecastVM>>(
-      () => _i59.ForecastToForecastVMMapper(
-            gh<_i18.Mapper<_i57.Temperature, _i58.TemperatureVM>>(),
-            gh<_i18.Mapper<_i57.Wind, _i58.WindVM>>(),
-            gh<_i18.Mapper<_i57.Weather, _i58.WeatherVM>>(),
-            gh<_i60.HumidityStringProvider>(),
-            gh<_i61.DateTimeStringProvider>(),
-          ));
-  gh.factory<_i62.SplashBloc>(() => _i62.SplashBloc(
-        gh<_i32.SplashStringProvider>(),
-        gh<_i6.DateTimeProvider>(),
-      ));
-  gh.singleton<_i63.AuthBloc>(_i63.AuthBloc(
-    gh<_i45.AuthService>(),
-    gh<_i40.AuthCoordinator>(),
-    gh<_i64.AuthExceptionStringProvider>(),
-  ));
-  await gh.lazySingletonAsync<_i65.ForecastRepository>(
+  await gh.lazySingletonAsync<_i49.ForecastRepository>(
     () {
-      final i = _i66.ProdForecastRepository(
-        gh<_i31.OpenWeatherDataSource>(),
-        gh<_i18.Mapper<_i67.ForecastResponse, _i57.Forecast>>(),
+      final i = _i50.ProdForecastRepository(
+        gh<_i34.OpenWeatherDataSource>(),
+        gh<_i18.Mapper<_i51.ForecastResponse, _i52.Forecast>>(),
       );
       return i.init().then((_) => i);
     },
     registerFor: {_prod},
     preResolve: true,
   );
-  gh.singleton<_i68.ForecastService>(_i68.ForecastService(
+  gh.singleton<_i53.ForecastService>(_i53.ForecastService(
     gh<_i16.LocationRepository>(),
-    gh<_i65.ForecastRepository>(),
+    gh<_i49.ForecastRepository>(),
+  ));
+  gh.lazySingleton<_i18.Mapper<_i20.Wind, _i54.WindVM>>(
+      () => _i55.WindToWindVMMapper(
+            gh<_i56.WindDirectionStringProvider>(),
+            gh<_i41.WindStringProvider>(),
+          ));
+  gh.lazySingleton<_i18.Mapper<_i23.Weather, _i57.WeatherVM>>(
+      () => _i58.WeatherToWeatherVMMapper(gh<_i59.WeatherIconProvider>()));
+  gh.lazySingleton<_i18.Mapper<_i26.Temperature, _i60.TemperatureVM>>(() =>
+      _i61.TemperatureToTemperatureVMMapper(
+          gh<_i37.TemperatureStringProvider>()));
+  gh.lazySingleton<_i18.Mapper<_i52.Forecast, _i62.ForecastVM>>(
+      () => _i63.ForecastToForecastVMMapper(
+            gh<_i18.Mapper<_i52.Temperature, _i62.TemperatureVM>>(),
+            gh<_i18.Mapper<_i52.Wind, _i62.WindVM>>(),
+            gh<_i18.Mapper<_i52.Weather, _i62.WeatherVM>>(),
+            gh<_i64.HumidityStringProvider>(),
+            gh<_i65.DateTimeStringProvider>(),
+          ));
+  gh.factory<_i66.SplashBloc>(() => _i66.SplashBloc(
+        gh<_i35.SplashStringProvider>(),
+        gh<_i6.DateTimeProvider>(),
+      ));
+  gh.singleton<_i67.AuthBloc>(_i67.AuthBloc(
+    gh<_i48.AuthService>(),
+    gh<_i43.AuthCoordinator>(),
+    gh<_i68.AuthExceptionStringProvider>(),
   ));
   gh.factory<_i69.ForecastBloc>(() => _i69.ForecastBloc(
-        gh<_i68.ForecastService>(),
+        gh<_i53.ForecastService>(),
         gh<_i70.ForecastExceptionStringProvider>(),
-        gh<_i18.Mapper<_i47.Forecast, _i58.ForecastVM>>(),
+        gh<_i18.Mapper<_i32.Forecast, _i62.ForecastVM>>(),
       ));
   return getIt;
 }
--- lib/presentation/screens/forecast_screen.dart
+++ lib/presentation/screens/forecast_screen.dart
@@ -7,6 +7,8 @@ import '../../gen/assets.gen.dart';
 import '../../themes/extensions/extensions.dart';
 import '../widgets/widgets.dart';
 
+const _paddingA = 24.0;
+
 class ForecastScreen extends StatelessWidget {
   const ForecastScreen({super.key});
 
@@ -29,12 +31,15 @@ class ForecastScreen extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
         ),
-        child: const Scaffold(
+        child: Scaffold(
           backgroundColor: Colors.transparent,
           body: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.all(24),
-              child: _Body(),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return _Body(
+                  maxHeight: constraints.maxHeight,
+                );
+              },
             ),
           ),
         ),
@@ -44,13 +49,18 @@ class ForecastScreen extends StatelessWidget {
 }
 
 class _Body extends StatelessWidget {
-  const _Body();
+  const _Body({
+    required this.maxHeight,
+  });
+
+  final double maxHeight;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final scaffoldMsg = ScaffoldMessenger.of(context);
     final bloc = context.read<ForecastBloc>();
+    final height = maxHeight - 2 * _paddingA;
 
     return BlocConsumer<ForecastBloc, ForecastState>(
       listener: (context, state) {
@@ -64,87 +74,114 @@ class _Body extends StatelessWidget {
       },
       builder: (context, state) {
         if (state.isLoading) {
-          return const Center(
-            child: LoadingIndicator(),
+          return const Padding(
+            padding: EdgeInsets.all(_paddingA),
+            child: Center(
+              child: LoadingIndicator(),
+            ),
           );
         }
 
         final forecast = state.selectedForecast;
         if (forecast == null) {
-          return const Center(
-            child: Text('Нет данных'),
+          return RefreshIndicator(
+            onRefresh: () async {
+              bloc.add(const ForecastEvent.refresh());
+            },
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              padding: const EdgeInsets.all(_paddingA),
+              child: SizedBox(
+                height: height,
+                child: const Center(
+                  child: Text('Нет данных'),
+                ),
+              ),
+            ),
           );
         }
 
-        return Column(
-          children: [
-            InkWell(
-              onTap: () {},
-              child: Row(
-                children: [
-                  Assets.icons.location.svg(),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    state.location,
-                    style: theme.textTheme.titleSmall,
-                  ),
-                ],
-              ),
-            ),
-            DecoratedBox(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: RadialGradient(
-                  colors: [
-                    theme.colorScheme.secondary,
-                    theme.colorScheme.secondary.withOpacity(0),
-                  ],
-                  stops: const [0.01, 1],
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(32),
-                child: Image.asset(forecast.weather.bigIconPath),
-              ),
-            ),
-            Expanded(
+        return RefreshIndicator(
+          onRefresh: () async {
+            bloc.add(const ForecastEvent.refresh());
+          },
+          child: SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            padding: const EdgeInsets.all(_paddingA),
+            child: SizedBox(
+              height: height,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    forecast.temperature.current,
-                    style: theme.textTheme.displayLarge,
+                  InkWell(
+                    onTap: () {},
+                    child: Row(
+                      children: [
+                        Assets.icons.location.svg(),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          state.location,
+                          style: theme.textTheme.titleSmall,
+                        ),
+                      ],
+                    ),
                   ),
-                  Text(
-                    forecast.weather.description,
-                    style: theme.textTheme.bodyLarge,
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: RadialGradient(
+                        colors: [
+                          theme.colorScheme.secondary,
+                          theme.colorScheme.secondary.withOpacity(0),
+                        ],
+                        stops: const [0.01, 1],
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(32),
+                      child: Image.asset(forecast.weather.bigIconPath),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          forecast.temperature.current,
+                          style: theme.textTheme.displayLarge,
+                        ),
+                        Text(
+                          forecast.weather.description,
+                          style: theme.textTheme.bodyLarge,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          forecast.temperature.minMax,
+                          style: theme.textTheme.bodyLarge,
+                        ),
+                      ],
+                    ),
+                  ),
+                  ForecastDayCard(
+                    onItemPressed: (index) {
+                      bloc.add(ForecastEvent.selectForecast(index: index));
+                    },
+                    selectedForecastIndex: state.selectedForecastIndex,
+                    forecasts: state.forecasts,
                   ),
                   const SizedBox(
-                    height: 10,
+                    height: 24,
                   ),
-                  Text(
-                    forecast.temperature.minMax,
-                    style: theme.textTheme.bodyLarge,
+                  ForecastDetailsDayCard(
+                    forecast: forecast,
                   ),
                 ],
               ),
             ),
-            ForecastDayCard(
-              onItemPressed: (index) {
-                bloc.add(ForecastEvent.selectForecast(index: index));
-              },
-              selectedForecastIndex: state.selectedForecastIndex,
-              forecasts: state.forecasts,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            ForecastDetailsDayCard(
-              forecast: forecast,
-            ),
-          ],
+          ),
         );
       },
     );
